51bc194fb94e1ca802ddd3d9b23692dc8da8f833
ben-dreher <benjamin.dreher@dominos.com>
2025-02-20 15:18:35 -0500

    remove alb target

--- main.tf
+++ main.tf
@@ -42,9 +42,9 @@ module "autoscaling" {
   min_size = 1
   max_size = 2
 
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
+  vpc_zone_identifier                   = module.blog_vpc.public_subnets
+  autoscaling_group_target_group_arns   = module.blog_alb.target_group_arns
+  security_groups                       = [module.blog_sg.security_group_id]
   
   image_id           = data.aws_ami.app_ami.id
   instance_type      = var.instance_type
@@ -67,7 +67,6 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
   }
 
