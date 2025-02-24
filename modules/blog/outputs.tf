b0716363440de5de1e9f0125ae2b4324f58db885
ben-dreher <benjamin.dreher@dominos.com>
2025-02-24 15:28:13 -0500

    Fix

--- modules/blog/outputs.tf
+++ modules/blog/outputs.tf
@@ -1,6 +1,3 @@
 output "environment_url" {
-    source  = "terraform-aws-modules/alb/aws"
-    version = "7.0.0"
-    
-    value = module.blog_alb.lb_dns_name
+    value = module.blog_alb.dns_name
 }
